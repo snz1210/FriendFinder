1474e2833e168d254cfbc4dd2412ebdab6212d00
Saima Zia <saima@Saimas-MacBook-Pro.local>
2018-01-21 21:24:18 -0500

    working on apiRoutes

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+
+	{
+		name: 'Ron Burgandy',
+		photoURL: 'https://pbs.twimg.com/profile_images/1990249248/image.jpg',
+		surveyResponses: [2, 5, 1, 3, 2, 4, 1, 2, 1, 1]
+
+	}, {
+		name: 'Rick Grimes',
+		photoURL: 'https://undeadwalking.com/wp-content/blogs.dir/318/files/2014/12/tumblr_nfvsj4l1vV1qzb6bqo4_1280rick1.jpg',
+		surveyResponses: [2, 2, 1, 3, 2, 4, 5, 5, 1, 1]
+
+	}, {
+		name: 'Michael Scott',
+		photoURL: 'http://www.etonline.com/sites/default/files/images/2017-01/1280_Steve_Carell_Michael-Scott_TheOffice_NBC.jpg',
+		surveyResponses: [1, 2, 1, 5, 1, 4, 4, 1, 3, 3]
+
+	}, {
+		name: 'Zombie',
+		photoURL: 'http://ksassets.timeincuk.net/wp/uploads/sites/54/2016/02/zombies-3.jpg',
+		surveyResponses: [2, 4, 4, 1, 2, 5, 5, 1, 2, 1]
+	}, {
+		name: 'Leloo Dallas Multipass',
+		photoURL: 'https://i.imgur.com/muvswfA.jpg',
+		surveyResponses: [5, 1, 2, 2, 1, 1, 5, 5, 5, 1]
+	}, {
+		name: 'Hellraiser',
+		photoURL: 'http://cdn.bloody-disgusting.com/wp-content/uploads/2016/08/Hellraiser.jpeg',
+		surveyResponses: [3, 4, 1, 5, 1, 3, 2, 2, 1, 1]
+	}
+
+
+
+];
+
+module.exports = friends;
